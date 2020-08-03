6d2609dcbee3aeaac2800ee80e5f1f6781e91a75
alister <alister.messi@gmail.com>
2020-08-03 22:16:27 +0100

    Completed Context API

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'; 
+import { useStateValue } from './StateProvider';
 
 function Header() {
+    const [{basket}] = useStateValue();
     return (
         <nav className="header">
             {/*Logo on left img*/}
@@ -50,7 +52,7 @@ function Header() {
                         {/*Shopping Basket Icon*/}
                         <ShoppingBasketIcon />
                         {/*Number of items in the Basket*/}
-                        <span className="header__optionLineTwo header__basketCount">0</span>
+                        <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
                     </div>
                 </Link>
             </div>
